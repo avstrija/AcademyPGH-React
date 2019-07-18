abc2a5a14a98a41a1c7943c0f763220c62bd5b58
dasha_bo_or <sonja.rosenfeld@gmail.com>
2019-07-17 20:40:25 -0400

    added a project

---
+++ mondrian/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="mond">
+        <div id="top-rect1"  style={{backgroundColor:'white'}}></div>
+        <div id="top-rect2"  style={{backgroundColor:'white'}}></div>
+        <div id="top-rect3"   style={{backgroundColor:'goldenrod'}}></div>
+        <div id="top-rect4"   style={{backgroundColor:'white'}}></div>
+        <div id="row2-rect5"  style={{backgroundColor:'white'}}></div>
+        <div id="red-square"  style={{backgroundColor:'red'}}></div>
+        <div id="row2-yellow-rect6" style={{backgroundColor:'goldenrod'}}></div>
+        <div id="vert-white-rect7" style={{backgroundColor:'white'}}></div>
+        <div id="vert-white-rect8" style={{backgroundColor:'white'}}></div>
+        <div id="vert-white-rect9" style={{backgroundColor:'white'}}></div>
+        <div id="black-square" style={{backgroundColor:'black'}}></div>
+        <div id="vert-white-rect11" style={{backgroundColor:'white'}}></div>
+        <div id="vert-white-rect12" style={{backgroundColor:'white'}}></div>
+        <div id="hor-white-rect13" style={{backgroundColor:'white'}}></div>
+        <div id="hor-yellow-rect14" style={{backgroundColor:'goldenrod'}}></div>
+        <div id="hor-white-rect15" style={{backgroundColor:'white'}}></div>
+        <div id="hor-black-rect16" style={{backgroundColor:'black'}}></div>
+        <div id="hor-white-rect17" style={{backgroundColor:'white'}}></div>
+        <div id="blue-rect" style={{backgroundColor:'darkblue'}}></div>
+        <div id="red-rect" style={{backgroundColor:'red'}}></div>
+      </div>
+  );
+}
+
+export default App;
